42bc16d88640ad58d7ef88531ca8c047d4001262
Glitch752 <xxGlitch752xx@gmail.com>
2026-02-01 04:07:03 +0800

    Mimi and all the other things

--- scenes/level.gd
+++ scenes/level.gd
@@ -2,77 +2,10 @@
 
 extends Node2D
 
-@export_tool_button("Generate map from polygon")
-var generate_map_from_polygon = _generate_map_from_polygon
+@export_tool_button("Generate map")
+var generate_map = _generate_map
 
-@export var map_scale_factor: float = 1.0
+@export var map_size: Vector2i = Vector2i.ONE * 20
 
-func _generate_map_from_polygon():
-    var points: PackedVector2Array = []
-    
-    var file = FileAccess.open("res://misc/geojson_parsing/sg_poly.txt", FileAccess.READ)
-    if file:
-        while not file.eof_reached():
-            var line = file.get_line()
-            var coords = line.split(" ")
-            if coords.size() == 2:
-                var x = float(coords[0])
-                var y = float(coords[1])
-                # Flip y
-                points.append(Vector2(x * map_scale_factor, -y * map_scale_factor))
-        file.close()
-    
-    # Don't duplicate the first and last point
-    if points.size() > 1 and points[0] == points[points.size() - 1]:
-        points.resize(points.size() - 1)
-    
-    print("Loaded %d points from polygon file" % points.size())
-
-
-    # The hard part: prevent overlapping line segments by simplifying the polygon in those situations
-    points = _remove_overlapping_segments(points)
-
-    $%Landmass.polygon = points
-    # Copy to the collision
-    $%LandmassCollision.polygon = points
-
-    # Set UVs to a scaled version of the position
-    var uvs: PackedVector2Array = []
-    for point in points:
-        uvs.append(point * 0.01 / map_scale_factor)
-    $%Landmass.uv = uvs
-
-func _remove_overlapping_segments(points: PackedVector2Array) -> PackedVector2Array:
-    if points.size() < 3:
-        return points.duplicate()
-
-    var segments_intersect = func(a1, a2, b1, b2):
-        # Returns true if segment a1-a2 intersects b1-b2
-        var d = (a2.x - a1.x) * (b2.y - b1.y) - (a2.y - a1.y) * (b2.x - b1.x)
-        if d == 0:
-            return false # Parallel
-        var u = ((b1.x - a1.x) * (b2.y - b1.y) - (b1.y - a1.y) * (b2.x - b1.x)) / d
-        var v = ((b1.x - a1.x) * (a2.y - a1.y) - (b1.y - a1.y) * (a2.x - a1.x)) / d
-        return (u > 0 and u < 1 and v > 0 and v < 1)
-
-    var cleaned = points.duplicate()
-    var changed = true
-    while changed:
-        changed = false
-        var n = cleaned.size()
-        for i in range(n):
-            var a1 = cleaned[i]
-            var a2 = cleaned[(i + 1) % n]
-            for j in range(i + 2, i + n - 1):
-                var idx1 = j % n
-                var idx2 = (j + 1) % n
-                var b1 = cleaned[idx1]
-                var b2 = cleaned[idx2]
-                if segments_intersect.call(a1, a2, b1, b2):
-                    # Remove the start point of the second segment (b1)
-                    cleaned.remove_at(idx1)
-                    changed = true
-                    break
-            if changed:
-                break
-    return cleaned
+func _generate_map():
+    pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,23 +2,44 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const ACCEL = 1800.0
 
 func _physics_process(delta: float) -> void:
-	var directionX := Input.get_axis("left","right")
-	var directionY := Input.get_axis("up","down")
-	if directionX:
-		velocity.x = directionX*SPEED
-		if directionX > 0:
-			$Sprite2D.flip_h = true
-		else:
-			$Sprite2D.flip_h = false;
-	else:
-		velocity.x = move_toward(velocity.x,0,SPEED)
-	if directionY:
-		velocity.y = directionY*SPEED
-	else:
-		velocity.y = move_toward(velocity.x,0,SPEED)
+    var directionX := Input.get_axis("left","right")
+    var directionY := Input.get_axis("up","down")
+    if directionX:
+        velocity.x = move_toward(velocity.x, directionX * SPEED, ACCEL * delta)
+        if directionX > 0:
+            $Sprite2D.flip_h = true
+        else:
+            $Sprite2D.flip_h = false
+    else:
+        velocity.x = move_toward(velocity.x, 0, ACCEL * delta)
+    if directionY:
+        velocity.y = move_toward(velocity.y, directionY * SPEED, ACCEL * delta)
+    else:
+        velocity.y = move_toward(velocity.y, 0, ACCEL * delta)
+    
+    var time = Time.get_ticks_msec() / 1000.0
+    $Sprite2D.rotation = sin(time * 20.0) * (velocity.length() / SPEED) * deg_to_rad(5)
 
-	move_and_slide()
+    if velocity.length() > SPEED * 0.1:
+        # Set animtion to the nearest direction
+        var direction_animations = {
+            Vector2.UP: "up",
+            Vector2.DOWN: "down",
+            Vector2.LEFT: "side",
+            Vector2.RIGHT: "side"
+        }
+
+        var nearest_dir = Vector2.UP
+        var nearest_dot = -1.0
+        for dir in direction_animations.keys():
+            var dot = dir.dot(velocity.normalized())
+            if dot > nearest_dot:
+                nearest_dot = dot
+                nearest_dir = dir
+        
+        $Sprite2D.animation = direction_animations[nearest_dir]
+
+    move_and_slide()
