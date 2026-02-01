30e5ffc37fc497e73b60a7a28ccd478734402b02
CattacoCattaco <cattacoiscool@gmail.com>
2026-02-01 09:00:52 -0800

    Add Interaction Area
    Added interaction area to buildings

--- scenes/level/buildings/base_building.gd
+++ scenes/level/buildings/base_building.gd
@@ -3,14 +3,17 @@
 extends Node2D
 
 enum Purpose {
-    SHOP,
-    DESTINATION,
-    DECORATION,
-    MRT,
+	SHOP,
+	DESTINATION,
+	DECORATION,
+	MRT,
 }
 
 # hide when the player goes behind it
 @onready var hide_area: Area2D = $HideArea
+
+# interact when the player goes near it
+@onready var interaction_area: Area2D = $InteractionArea
 
 @export var tile_size: Vector2i = Vector2i.ONE
 
@@ -21,24 +24,24 @@ enum Purpose {
 
 
 func get_tile_position() -> Vector2i:
-    var tilemap: TileMapLayer = get_parent()
-    return tilemap.local_to_map(position)
+	var tilemap: TileMapLayer = get_parent()
+	return tilemap.local_to_map(position)
 
 func _ready() -> void:
-    if not Engine.is_editor_hint():
-        hide_area.body_entered.connect(_on_body_entered)
-        hide_area.body_exited.connect(_on_body_exited)
+	if not Engine.is_editor_hint():
+		hide_area.body_entered.connect(_on_body_entered)
+		hide_area.body_exited.connect(_on_body_exited)
 
 func _on_body_entered(body: Node) -> void:
-    if body.is_in_group("player"):
-        var tween = create_tween()
-        tween.set_ease(Tween.EASE_IN_OUT)
-        tween.set_trans(Tween.TRANS_CUBIC)
-        tween.tween_property(self, "modulate:a", 0.3, 0.3)
+	if body.is_in_group("player"):
+		var tween = create_tween()
+		tween.set_ease(Tween.EASE_IN_OUT)
+		tween.set_trans(Tween.TRANS_CUBIC)
+		tween.tween_property(self, "modulate:a", 0.3, 0.3)
 
 func _on_body_exited(body: Node) -> void:
-    if body.is_in_group("player"):
-        var tween = create_tween()
-        tween.set_ease(Tween.EASE_IN_OUT)
-        tween.set_trans(Tween.TRANS_CUBIC)
-        tween.tween_property(self, "modulate:a", 1.0, 0.3)
+	if body.is_in_group("player"):
+		var tween = create_tween()
+		tween.set_ease(Tween.EASE_IN_OUT)
+		tween.set_trans(Tween.TRANS_CUBIC)
+		tween.tween_property(self, "modulate:a", 1.0, 0.3)
