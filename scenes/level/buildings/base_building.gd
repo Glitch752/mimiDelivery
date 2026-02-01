60e7d71e903fa130e2edf20baa66b647db0bb1e0
Glitch752 <xxGlitch752xx@gmail.com>
2026-02-01 08:40:24 +0800

    whitespace

--- scenes/level/buildings/base_building.gd
+++ scenes/level/buildings/base_building.gd
@@ -3,10 +3,10 @@
 extends Node2D
 
 enum Purpose {
-	SHOP,
-	DESTINATION,
-	DECORATION,
-	MRT,
+    SHOP,
+    DESTINATION,
+    DECORATION,
+    MRT,
 }
 
 # hide when the player goes behind it
@@ -30,15 +30,15 @@ func _ready() -> void:
         hide_area.body_exited.connect(_on_body_exited)
 
 func _on_body_entered(body: Node) -> void:
-	if body.is_in_group("player"):
-		var tween = create_tween()
-		tween.set_ease(Tween.EASE_IN_OUT)
-		tween.set_trans(Tween.TRANS_CUBIC)
-		tween.tween_property(self, "modulate:a", 0.3, 0.3)
+    if body.is_in_group("player"):
+        var tween = create_tween()
+        tween.set_ease(Tween.EASE_IN_OUT)
+        tween.set_trans(Tween.TRANS_CUBIC)
+        tween.tween_property(self, "modulate:a", 0.3, 0.3)
 
 func _on_body_exited(body: Node) -> void:
-	if body.is_in_group("player"):
-		var tween = create_tween()
-		tween.set_ease(Tween.EASE_IN_OUT)
-		tween.set_trans(Tween.TRANS_CUBIC)
-		tween.tween_property(self, "modulate:a", 1.0, 0.3)
+    if body.is_in_group("player"):
+        var tween = create_tween()
+        tween.set_ease(Tween.EASE_IN_OUT)
+        tween.set_trans(Tween.TRANS_CUBIC)
+        tween.tween_property(self, "modulate:a", 1.0, 0.3)
