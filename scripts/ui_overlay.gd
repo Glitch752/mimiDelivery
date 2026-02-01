cb7fea7a08467e2007cf992d9f1c56c9c13fbef1
CattacoCattaco <cattacoiscool@gmail.com>
2026-01-31 16:38:16 -0800

    Add UI to Mimi Scene
    Added the tasks panel and time display as children of the camera in the mimi scene

---
+++ scripts/ui_overlay.gd
@@ -0,0 +1,10 @@
+class_name UIOverlay
+extends Control
+
+@export var camera: Camera2D
+
+
+func _ready() -> void:
+	size = get_viewport_rect().size
+	scale = Vector2(1.0 / camera.zoom.x, 1.0 / camera.zoom.y)
+	position = -(size / 2.0) / camera.zoom
